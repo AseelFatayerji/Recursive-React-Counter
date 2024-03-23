b77385a0ef16b53c0818de8f2f343b86432042ea
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-03-23 12:38:31 +0200

    Created add and remove functions

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -2,13 +2,29 @@ import "../CSS/App.css";
 
 function App() {
 
+  const add = (item) => {
+    const div = item.target.parentElement;
+    const id = parseInt(div.getAttribute("postid")) + div.children.length - 1;
+    const newElement = createElement(id);
+    div.appendChild(newElement);
+  };
+  const remove = (item) => {
+    const div = item.target.parentElement;
+    if (div.parentElement.children.length != 1) {
+      div.remove();
+    }
+  };
   return (
     <div className="App">
       <div className="body" id="body">
-          <div postid="-1">
+        <div postid="-1">
+          <label>-1</label> <button onClick={add}>+</button>
+          <button onClick={remove}>-</button>
+          <div className="margin" postid="-1">
             <label>-1</label> <button onClick={add}>+</button>
             <button onClick={remove}>-</button>
           </div>
+        </div>
       </div>
     </div>
   );
